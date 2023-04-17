c79172890d70716da6f8267688954f2a3a9957c8
Blake Combden <blake.combden27@ed.cna.nl.ca>
2023-04-17 14:17:51 -0230

    Added API routes for adding, overriding, and deleting data in database.

--- webapp-backend/src/server.js
+++ webapp-backend/src/server.js
@@ -18,6 +18,7 @@ app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
+// get all questions in db
 app.get('/api/questions', async (req, res) => {
     
     // Create client object and wait for connection
@@ -27,8 +28,57 @@ app.get('/api/questions', async (req, res) => {
     const db = client.db('triviapp');
     // Pull data from db and store
     const questions = await db.collection('questions').find({}).toArray();
-    console.log(questions);
     res.json(questions);
+});
+
+// add any number of questions to db
+app.post('/api/append', async (req, res) => {
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    await client.connect();
+    const db = client.db('triviapp');
+
+    let data = req.body;
+
+    try{
+        await db.collection('questions').insertMany(data);
+        res.json(data);
+    } catch (e) {
+        console.log("Error");
+        console.log(e);
+    }
+});
+
+// override db with any number of questions
+app.post('/api/override', async (req, res) => {
+
+    // Create client object and wait for connection
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    await client.connect();
+    // Set database
+    const db = client.db('triviapp');
+    // Remove all data from the database
+    await db.collection('questions').deleteMany();
+    res.sendStatus(200);
+
+    //Repopulate the database
+    let data = req.body;
+
+    try{
+        await db.collection('questions').insertMany(data);
+        res.sendStatus(200);
+    } catch (e) {
+        console.log("Error");
+        console.log(e);
+    }
+});
+
+// delete all questions from db
+app.post('/api/removeAll', async (req, res) => {
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    await client.connect();
+    const db = client.db('triviapp');
+    await db.collection('questions').deleteMany();
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
